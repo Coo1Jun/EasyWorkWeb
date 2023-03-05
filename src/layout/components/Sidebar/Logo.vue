93f9067fc0f77e9f78286b36dbd1d609f997c328
CoolJun <1425176577@qq.com>
2023-03-05 16:25:46 +0800

    feat:侧边栏显示logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       title: 'Easy Work',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/ew_logo_bfcdd9.png'
     }
   }
 }
@@ -44,15 +44,16 @@ export default {
 .sidebar-logo-container {
   position: relative;
   width: 100%;
-  height: 50px;
+  height: 70px;
   line-height: 50px;
-  background: #2b2f3a;
+  background: #304156;
   text-align: center;
   overflow: hidden;
 
   & .sidebar-logo-link {
     height: 100%;
     width: 100%;
+    line-height: 70px;
 
     & .sidebar-logo {
       width: 32px;
@@ -64,10 +65,10 @@ export default {
     & .sidebar-title {
       display: inline-block;
       margin: 0;
-      color: #fff;
+      color: #bfcdd9;
       font-weight: 600;
-      line-height: 50px;
-      font-size: 14px;
+      // line-height: 70px;
+      font-size: 20px;
       font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
       vertical-align: middle;
     }
