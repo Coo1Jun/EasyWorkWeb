e3fe71bcf653b82b8d6bd80ee08e92ecd0627e47
CoolJun <1425176577@qq.com>
2023-03-10 21:19:15 +0800

    feat:个人资料添加个人信息、密码、邮箱的修改

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -7,14 +7,16 @@
       <div style="margin-top: 10px;margin-bottom: 10px" class="text">
         选择一张个人正面照片作为头像，企业成员可以更容易认识你
       </div>
-      <el-avatar style="margin-right: 10px" :size="150" :src="imageUrl" />
+      <el-avatar style="margin-top: 10px;margin-bottom: 10px" :size="150" :src="imageUrl" />
 
-      <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">
-        修改头像
-      </el-button>
-      <span style="color: #c6bec9;margin-left: 10px;font-size: 14px">
-        仅支持JPG,PNG格式上传
-      </span>
+      <div>
+        <el-button style="width: 150px" type="primary" icon="el-icon-upload" @click="imagecropperShow=true">
+          修改头像
+        </el-button>
+        <span style="color: #c6bec9;margin-left: 10px;font-size: 14px">
+          仅支持JPG,PNG格式上传
+        </span>
+      </div>
 
       <image-cropper
         v-show="imagecropperShow"
@@ -27,8 +29,155 @@
         @crop-upload-success="cropSuccess"
       />
     </div>
-    <div>
-      hello
+
+    <div class="profile-container">
+      <div class="text" style="margin-bottom: 20px">
+        个人资料
+      </div>
+      <div style="width: 500px">
+        <el-form
+          ref="profile"
+          :model="profile"
+          label-width="100px"
+          :rules="profileRules"
+        >
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="profile.name" />
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="profile.email" :disabled="true" />
+          </el-form-item>
+          <el-form-item label="出生日期">
+            <el-date-picker
+              v-model="profile.birthday"
+              type="date"
+              placeholder="选择日期"
+              style="width: 100%;"
+              format="yyyy 年 MM 月 dd 日"
+              value-format="yyyy-MM-dd"
+            />
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio-group v-model="profile.sex">
+              <el-radio label="0">男</el-radio>
+              <el-radio label="1">女</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="个性签名">
+            <el-input v-model="profile.desc" type="textarea" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" :loading="loading" @click="saveSetting">保存设置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+
+    <div class="other-container">
+      <div class="other-item">
+        <div class="other-item-title">
+          修改密码
+          <el-link v-if="!editPasswordShow" style="font-size: 16px;float: right;margin-right: 30px" type="primary" @click="editPasswordShow=!editPasswordShow">
+            展开
+            <i class="el-icon-caret-bottom" />
+          </el-link>
+          <el-link v-else style="font-size: 16px;float: right;margin-right: 30px" type="primary" @click="editPasswordShow=!editPasswordShow">
+            收起
+            <i class="el-icon-caret-top" />
+          </el-link>
+        </div>
+        <div class="other-item-body">
+          修改密码时需要输入当前密码，如果你忘记了当前密码，可以点击这里
+          <el-link style="font-size: 16px" type="primary" @click="toForgot">通过邮件重置</el-link>
+          你的密码。
+        </div>
+        <el-form
+          v-if="editPasswordShow"
+          ref="editPassword"
+          style="margin-top: 20px"
+          :model="editPassword"
+          :rules="editPasswordRules"
+        >
+          <el-form-item prop="oldPassword">
+            <el-input
+              v-model="editPassword.oldPassword"
+              class="other-input"
+              placeholder="旧密码"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item prop="newPassword">
+            <el-input
+              v-model="editPassword.newPassword"
+              class="other-input"
+              placeholder="新密码"
+              show-password
+            />
+          </el-form-item>
+          <el-button
+            class="btn-save"
+            :loading="editPwdloading"
+            type="primary"
+            @click="savePassword"
+          >保存</el-button>
+        </el-form>
+      </div>
+      <div class="other-item">
+        <div class="other-item-title">
+          修改邮箱
+          <el-link v-if="!editEmailShow" style="font-size: 16px;float: right;margin-right: 30px" type="primary" @click="editEmailShow=!editEmailShow">
+            展开
+            <i class="el-icon-caret-bottom" />
+          </el-link>
+          <el-link v-else style="font-size: 16px;float: right;margin-right: 30px" type="primary" @click="editEmailShow=!editEmailShow">
+            收起
+            <i class="el-icon-caret-top" />
+          </el-link>
+        </div>
+        <div class="other-item-body">
+          你当前的邮箱是
+          <span style="color: #ff7575">{{ userInfo.email }}</span>
+          ，修改邮箱时系统会发送验证码到新的邮箱上，然后输入验证码后完成修改。
+        </div>
+        <el-form
+          v-if="editEmailShow"
+          ref="editEmail"
+          :model="editEmail"
+          :rules="editEmailRules"
+          style="margin-top: 20px"
+        >
+          <el-form-item prop="email">
+            <el-input
+              v-model="editEmail.email"
+              class="other-input"
+              placeholder="请输入邮箱"
+            />
+          </el-form-item>
+          <el-form-item prop="code">
+            <el-input
+              v-model="editEmail.code"
+              class="other-input"
+              placeholder="请输入验证码"
+            >
+              <template>
+                <el-button
+                  v-if="canSend"
+                  slot="append"
+                  class="btn-code"
+                  @click.native.prevent="sendCode"
+                >获取验证码</el-button>
+                <template v-else slot="append">已发送，{{ timeLeft }}秒后重新获取</template>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-button
+            :loading="loading"
+            class="btn-save"
+            type="primary"
+            @click.native.prevent="handleBind"
+          >绑定</el-button>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
@@ -36,15 +185,102 @@
 <script>
 import ImageCropper from '@/components/ImageCropper'
 import { mapGetters } from 'vuex'
-import { editUserInfo } from '@/api/user'
+import { editUserInfo, sendVerifyCode } from '@/api/user'
+import { Notification } from 'element-ui'
 
 export default {
   name: 'Account',
   components: { ImageCropper },
   data() {
+    const validateName = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('用户名不能为空'))
+      } else {
+        callback()
+      }
+    }
+    // 检查密码是否包含数字和字母
+    const pwdValid = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    const validatePassword = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('密码不能为空'))
+      } else if (!pwdValid.test(value)) {
+        callback(new Error('密码必须由数字和字母组合，且不能少于8位'))
+      } else {
+        callback()
+      }
+    }
+    const reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-]+)+$/
+    const validateEmail = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('请输入邮箱'))
+      } else if (!reg.test(value)) {
+        callback(new Error('请输入正确的邮箱格式'))
+      } else {
+        callback()
+      }
+    }
+    const validateCode = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('请输入验证码'))
+      } else if (value.length !== 6) {
+        callback(new Error('请输入6位数验证码'))
+      } else {
+        callback()
+      }
+    }
+    const validateOldPassword = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('密码不能为空'))
+      } else {
+        callback()
+      }
+    }
     return {
+      editPasswordShow: false,
+      editEmailShow: false,
       imagecropperShow: false,
-      imagecropperKey: 0
+      imagecropperKey: 0,
+      loading: false,
+      editPwdloading: false,
+      canSend: true,
+      timeLeft: 60,
+      profile: {
+        name: '',
+        email: '',
+        birthday: '',
+        sex: '',
+        desc: ''
+      },
+      profileRules: {
+        name: [
+          { required: true, trigger: 'blur', validator: validateName }
+        ]
+      },
+      editPassword: {
+        oldPassword: '',
+        newPassword: ''
+      },
+      editPasswordRules: {
+        oldPassword: [
+          { required: true, trigger: 'blur', validator: validateOldPassword }
+        ],
+        newPassword: [
+          { required: true, trigger: 'blur', validator: validatePassword }
+        ]
+      },
+      editEmail: {
+        email: '',
+        code: ''
+      },
+      editEmailRules: {
+        email: [
+          { required: true, trigger: 'blur', validator: validateEmail }
+        ],
+        code: [
+          { required: true, trigger: 'blur', validator: validateCode }
+        ]
+      }
     }
   },
   computed: {
@@ -54,6 +290,13 @@ export default {
     imageUrl() {
       return this.userInfo.portrait
     }
+  },
+  created() {
+    this.profile.name = this.userInfo.realName
+    this.profile.email = this.userInfo.email
+    this.profile.desc = this.userInfo.description
+    this.profile.birthday = this.userInfo.birthDate
+    this.profile.sex = this.userInfo.sex
   },
   methods: {
     cropSuccess(resData) {
@@ -67,6 +310,86 @@ export default {
     },
     close() {
       this.imagecropperShow = false
+    },
+    toForgot() {
+      window.open('#/forgot')
+    },
+    saveSetting() {
+      this.$refs.profile.validate(async(valid) => {
+        if (valid) {
+          this.loading = true
+          const requestData = {
+            id: this.userInfo.userid
+          }
+          if (this.profile.name !== this.userInfo.realName) {
+            requestData.realName = this.profile.name
+          }
+          if (this.profile.birthday !== this.userInfo.birthDate) {
+            requestData.birthDate = this.profile.birthday
+          }
+          if (this.profile.sex !== this.userInfo.sex) {
+            requestData.sex = this.profile.sex
+          }
+          if (this.profile.desc !== this.userInfo.description) {
+            requestData.description = this.profile.desc
+          }
+          if (Object.keys(requestData).length !== 1) {
+            const { data } = await editUserInfo(requestData).catch(() => {
+              this.loading = false
+            })
+            if (data) {
+              this.userInfo.realName = this.profile.name
+              this.userInfo.birthDate = this.profile.birthday
+              this.userInfo.sex = this.profile.sex
+              this.userInfo.description = this.profile.desc
+            }
+          } else {
+            console.log('数据没有被修改')
+          }
+          Notification({
+            type: 'success',
+            title: '更新个人资料成功',
+            duration: 3000
+          })
+          this.loading = false
+        } else {
+          this.loading = false
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    savePassword() {
+      this.$refs.editPassword.validate(valid => {
+        if (valid) {
+          this.editPwdloading = true
+          // todo
+          this.editPwdloading = false
+        } else {
+          this.editPwdloading = false
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    sendCode() {
+      this.$refs.editEmail.validateField('email', (errorMessage) => {
+        if (errorMessage) {
+          return false
+        } else {
+          sendVerifyCode({
+            email: this.editEmail.email,
+            type: 'editEmail'
+          }) // 发送验证码
+          this.canSend = false
+          this.intervalId = setInterval(() => {
+            this.timeLeft--
+          }, 1000)
+        }
+      })
+    },
+    handleBind() {
+      // todo
     }
   }
 }
@@ -78,9 +401,41 @@ export default {
   padding-bottom: 15px;
   border-bottom: 1px solid #eaeff1;
 }
+.profile-container {
+  margin: 40px 40px 20px;
+  border-bottom: 1px solid #eaeff1;
+}
+.other-container {
+  margin: 20px 40px 40px;
+}
+.other-item {
+  border-bottom: 1px solid #eaeff1;
+  padding-bottom: 15px;
+}
+.other-item-title {
+  margin: 10px 0;
+  font-weight: 700;
+  color: #4e545f;
+}
+.other-item-body {
+  color: #b5bec9;
+}
 .text {
   font-size: 14px;
   color: #333333;
 }
+.other-input {
+  width: 500px;
+}
+.btn-save {
+  width: 100px;
+}
+.btn-code {
+  background-color: #409eff !important;
+  color: #fff !important;
+}.btn-code:hover {
+  background-color: #66b1ff !important;
+}
+
 </style>
 
