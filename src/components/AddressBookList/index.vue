7986fca4adbf24103a6541424ccbe83cdc5a388d
CoolJun <1425176577@qq.com>
2023-04-21 00:09:34 +0800

    fix:【通讯录】使用props属性

--- src/components/AddressBookList/index.vue
+++ src/components/AddressBookList/index.vue
@@ -44,52 +44,14 @@
 export default {
   name: 'AddressBookList',
   components: { },
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
-      data: [
-        {
-          id: '1633362162592845826',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙'
-        },
-        {
-          id: '1557662367922384897',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙'
-        },
-        {
-          id: '1',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙'
-        },
-        {
-          id: '1',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙'
-        },
-        {
-          id: '1',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙'
-        },
-        {
-          id: '1',
-          name: '小明',
-          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
-          email: '142517@qq.com',
-          description: '我是一个帅小伙'
-        }
-      ]
     }
   },
   methods: {
--- src/views/addressbook/index.vue
+++ src/views/addressbook/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     AddressBook
-    <AddressBookList />
+    <AddressBookList :data="data" />
   </div>
 </template>
 
@@ -12,7 +12,50 @@ export default {
   components: { AddressBookList },
   data() {
     return {
-
+      data: [
+        {
+          id: '1633362162592845826',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙'
+        },
+        {
+          id: '1557662367922384897',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙'
+        },
+        {
+          id: '1',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙'
+        },
+        {
+          id: '1',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙我是一个帅小伙'
+        },
+        {
+          id: '1',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙'
+        },
+        {
+          id: '1',
+          name: '小明',
+          avatar: 'https://easywork23.oss-cn-shenzhen.aliyuncs.com/attachment/db02c8745a314524b18cbf5b9e310730.png',
+          email: '142517@qq.com',
+          description: '我是一个帅小伙'
+        }
+      ]
     }
   }
 }
