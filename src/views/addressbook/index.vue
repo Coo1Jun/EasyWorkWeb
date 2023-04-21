02c75de61953c24ab59af8255db989024ee663f1
CoolJun <1425176577@qq.com>
2023-04-21 13:54:34 +0800

    fix：【通讯录模块】对接项目组成员接口

--- src/views/addressbook/index.vue
+++ src/views/addressbook/index.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    AddressBook
-    <AddressBookList :data="data" />
+  <div class="ab-container">
+    <div class="ab-title-name">
+      项目组通讯录
+    </div>
+    <el-collapse v-model="groupActiveName" accordion>
+      <el-collapse-item v-for="g in groups" :key="g.id" :name="g.id">
+        <template slot="title">
+          <p style="font-size: 18px">{{ g.name }}</p>
+        </template>
+        <AddressBookList
+          v-loading="groupAddressBookLoading"
+          :data="groupAddressBookData"
+          enable-page
+          :total="groupSearch.total"
+          @page-change="handleGroupPageChange"
+        />
+      </el-collapse-item>
+    </el-collapse>
+    <div class="ab-title-name">
+      个人通讯录
+    </div>
+    <AddressBookList
+      :data="data"
+      enable-page
+      :total="personSearch.total"
+      @page-change="handlePersonPageChange"
+    />
   </div>
 </template>
 
 <script>
 import AddressBookList from '@/components/AddressBookList'
+import { getMemberListApi, getGroupListApi } from '@/api/group'
 export default {
   name: 'AddressBook',
   components: { AddressBookList },
@@ -55,12 +80,75 @@ export default {
           email: '142517@qq.com',
           description: '我是一个帅小伙'
         }
-      ]
+      ],
+      groupActiveName: '',
+      groups: [],
+      groupAddressBookData: [],
+      groupAddressBookLoading: false,
+      personSearch: {
+        name: '',
+        email: '',
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      },
+      groupSearch: {
+        name: '',
+        email: '',
+        currentPage: 1,
+        pageSize: 10,
+        total: 0
+      }
+    }
+  },
+  watch: {
+    groupActiveName(newVal) {
+      if (newVal !== '') {
+        this.handleGroupSearch()
+      }
+    }
+  },
+  async mounted() {
+    // 获取项目组信息列表
+    const groupResponse = await getGroupListApi()
+    this.groups = groupResponse.data.records
+  },
+  methods: {
+    handlePersonPageChange(currentPage, pageSize) {
+      this.personSearch.currentPage = currentPage
+      this.personSearch.pageSize = pageSize
+      console.log('当前页为=》', currentPage)
+      console.log('当前页大小为=》', pageSize)
+    },
+    handleGroupPageChange(currentPage, pageSize) {
+      this.groupSearch.currentPage = currentPage
+      this.groupSearch.pageSize = pageSize
+      this.handleGroupSearch()
+    },
+    async handleGroupSearch() {
+      this.groupAddressBookLoading = true
+      const memberList = await getMemberListApi({
+        groupId: this.groupActiveName,
+        name: this.groupSearch.name,
+        email: this.groupSearch.email,
+        limit: this.groupSearch.pageSize,
+        pageNo: this.groupSearch.currentPage
+      })
+      this.groupAddressBookData = memberList.data.records
+      this.groupSearch.total = memberList.data.total
+      this.groupAddressBookLoading = false
     }
   }
 }
 </script>
 
 <style scoped>
-
+.ab-container {
+  padding: 0 20px 40px 20px;
+}
+.ab-title-name {
+  font-size: 25px;
+  color: #333333;
+  margin: 30px 0;
+}
 </style>
